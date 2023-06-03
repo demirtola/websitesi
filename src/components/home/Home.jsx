2d83ea4e5e82f290790759219e73d13c6d5986e9
metehan demirtola <metehandemirtola@hotmail.com>
2023-06-04 02:28:09 +0300

    kuzuu

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import "./home.css";
 import Me from "../../assets/Me.svg";
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
+import Shapes from './Shapes';
 
 const Home = () => {
     return (
@@ -10,14 +11,16 @@ const Home = () => {
             <div className="intro"> 
               <img src={Me} alt="" className='home__img'/>
               <h1 className="home__name">Efsa</h1>
-              <span className="home__education">Software Developer, Designer, Lover</span>
+              <span className="home__education">Computer Engineer, Software Devoloper, Designer</span>
               <HeaderSocials/>   
             
               <a href="#contact" className="btn">Contact Me!</a>
+             
               <ScrollDown/>
             
             </div>
 
+            <Shapes/>
 
         </section>
     )
